acd59a3d7bb4d334f4efc3fe382b21c5ceeb435d
Pierre-Yves JACQUES <44160710+pyjacques@users.noreply.github.com>
2025-06-19 11:04:13 +0800

    Add animation controls

--- Total/js/viewer.js
+++ Total/js/viewer.js
@@ -17,5 +17,38 @@ document.addEventListener('DOMContentLoaded', () => {
       shadow-intensity="1"
       shadow-softness="1">
     </model-viewer>
+    <anim-button id="anim-button">
+      <button type="button" class="btn btn-primary text-light fs-3" onclick="separateView()">
+        <span data-bs-toggle="tooltip" data-bs-placement="right" title="separate-view"><i class="bi bi-layers-half"></i></span>
+      </button>
+    </anim-button>
   </viewer-container>`;
 });
+
+async function separateView() {
+  const modelViewer = document.querySelector('#modelViewer');
+  if (!modelViewer) return;
+  modelViewer.animationName = 'Explode';
+  await modelViewer.updateComplete;
+  modelViewer.timeScale = 1;
+  await modelViewer.play({ repetitions: 1 });
+  $('#anim-button').html(
+    `<button type="button" class="btn btn-primary text-light fs-3" onclick="initialView()">
+        <span data-bs-toggle="tooltip" data-bs-placement="right" title="initial-view"><i class="bi bi-box"></i></span>
+    </button>`
+  );
+}
+
+async function initialView() {
+  const modelViewer = document.querySelector('#modelViewer');
+  if (!modelViewer) return;
+  modelViewer.animationName = 'Mount';
+  await modelViewer.updateComplete;
+  modelViewer.timeScale = 1;
+  await modelViewer.play({ repetitions: 1 });
+  $('#anim-button').html(
+    `<button type="button" class="btn btn-primary text-light fs-3" onclick="separateView()">
+        <span data-bs-toggle="tooltip" data-bs-placement="right" title="separate-view"><i class="bi bi-layers"></i></span>
+    </button>`
+  );
+}
